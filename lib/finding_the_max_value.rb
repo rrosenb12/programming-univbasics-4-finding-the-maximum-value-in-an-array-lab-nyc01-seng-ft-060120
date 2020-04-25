e1f9c402e3a7a6076e47618a0d5943621754904f
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-25 19:35:29 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,6 +1,6 @@
 def find_max_value(array)
-  array.uniq
-  array.sort
-  return array.last
+  one_value_array = array.uniq
+  sorted_array = one_value_array.sort
+  return sorted_array.last
   # Add your solution here
 end
